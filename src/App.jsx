69aa38759800fd024da58a3b40034bfb36bcb6ba
NEM-ICU <itt1819025@tec.rjt.ac.lk>
2024-04-16 11:12:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import DragDropImageUploader from "./pages/DragDropImageUploader";
+import "./App.css";
+
+const App = () => {
+  return <DragDropImageUploader />;
+};
+
+export default App;
